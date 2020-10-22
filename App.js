81e75bf2db4f514cae37f0a4b38381bc2a63da82
Pasha <pasha.evstr@gmail.com>
2020-10-22 22:19:45 +0700

    изменил цвет фона

--- App.js
+++ App.js
@@ -4,9 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <Text style={ {color: '#fff'} }>это моё первое приложение</Text>
     </View>
   );
 }
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
