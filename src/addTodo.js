b0d4e661433ffbcd0095361275fab40032f3d4c9
Pasha <pasha.evstr@gmail.com>
2020-11-25 10:32:07 +0700

    вывод списка элементов

--- src/addTodo.js
+++ src/addTodo.js
@@ -1,11 +1,25 @@
-import React from "react";
-import { View, StayleSheet, TextInput, Button } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, TextInput, Button } from "react-native";
 
-export const addTodo = (props) => {
+const initialTodoTitle = "новая задача";
+const buttonTitle = "Добавить";
+
+export const AddTodo = (props) => {
+  const [currentTodo, setCurrentTodo] = useState(null);
+
+  const onClick = () => {
+    props.onSubmit(currentTodo || initialTodoTitle);
+    setCurrentTodo(null);
+  };
+
   return (
     <View style={styles.block}>
-      <TextInput />
-      <button title="добавить" />
+      <TextInput
+        onChangeText={(text) => setCurrentTodo(text)}
+        value={currentTodo}
+        placeholder={"Введите текст"}
+      />
+      <Button title={buttonTitle} onPress={onClick} />
     </View>
   );
 };
@@ -14,10 +28,11 @@ const styles = StyleSheet.create({
   block: {
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
   },
   input: {
-    widtborh: "70%",
-    borderStyle: "solid ",
+    width: "70%",
+    borderStyle: "solid",
     borderBottomWidth: 2,
     borderBottomColor: "#3949ab",
   },
